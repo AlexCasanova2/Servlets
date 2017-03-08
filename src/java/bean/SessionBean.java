e0162ea6cb8293aaf2d790d02b90176177be3736
Alex <casanovaalex11@gmail.com>
2017-03-08 17:23:04 +0100

    Servlet

--- src/java/bean/SessionBean.java
+++ src/java/bean/SessionBean.java
@@ -1,9 +1,11 @@
 package bean;
 
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
+import modelo.Pokemon;
 import modelo.Trainer;
 
 @Stateless
@@ -24,6 +26,32 @@ public class SessionBean {
         if(!existeEntrenador(t)){
             EntityManager em = emf.createEntityManager();
             em.persist(t);
+            em.close();
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public List<Trainer> selectAllEntrenadores(){
+        return emf.createEntityManager().createNamedQuery("Trainer.findAll").getResultList();
+    }
+    
+    public Trainer getTrainerByName(String name){
+        return emf.createEntityManager().find(Trainer.class, name);
+    }
+    
+    public boolean existePokemon(Pokemon p){
+        EntityManager em = emf.createEntityManager();
+        Pokemon encontrado = em.find(Pokemon.class, p.getName());
+        em.close();
+        return encontrado !=null;
+    }
+    
+    public boolean insertarPokemon(Pokemon p){
+        if(!existePokemon(p)){
+            EntityManager em = emf.createEntityManager();
+            em.persist(p);
             em.close();
             return true;
         }else{
--- src/java/servlets/AltaPokemon.java
+++ src/java/servlets/AltaPokemon.java
@@ -1,14 +1,22 @@
 package servlets;
 
+import bean.SessionBean;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelo.Pokemon;
+import modelo.Trainer;
 
 public class AltaPokemon extends HttpServlet {
 
+    
+    @EJB
+    SessionBean miEjb;
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -20,11 +28,54 @@ public class AltaPokemon extends HttpServlet {
             out.println("<head>");
             out.println("<title>Alta Pokemon</title>");            
             out.println("</head>");
-            out.println("<h1>Servlet AltaPokemon at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Alta Pokémon</h1>");
             out.println("<body>");
-            out.print("<input type=\"text\" name=\"nombre\" placeholder=\"nombre\">");
+            out.println("<form method=\"POST\">");
+            out.println("<input type=\"text\" name=\"nombre\" placeholder=\"nombre\">");
+            out.println("<br>");
+            out.println("<input type=\"text\" name=\"tipo\" placeholder=\"tipo\">");
+            out.println("<br>");
+            out.println("<input type=\"text\" name=\"habilidad\" placeholder=\"habilidad\">");
+            out.println("<br>");
+            out.println("<input type=\"number\" name=\"lvlataque\" placeholder=\"nivel ataque\">");
+            out.println("<br>");
+            out.println("<input type=\"number\" name=\"lvldef\" placeholder=\"nivel defensa\">");
+            out.println("<br>");
+            out.println("<input type=\"number\" name=\"velocidad\" placeholder=\"velocidad\">");
+            out.println("<br>");
+            out.println("<input type=\"number\" name=\"vida\" placeholder=\"vida\">");
+            out.println("<select name=\"entrenador\">");
+                List<Trainer> trainers = miEjb.selectAllEntrenadores();
+                    for(Trainer t : trainers){
+                        out.println("<option value=\"" + t.getName() + "\">" + t.getName() + "</option>");
+                    }
+            out.println("</select>");
+            out.println("<input type=\"submit\" name=\"alta\" value=\"Guardar\">");
+            out.println("</form>");
+            
+            if("Guardar".equals(request.getParameter("alta"))){
+            String name = request.getParameter("nombre");
+            String type = request.getParameter("tipo");
+            String hability = request.getParameter("habilidad");
+            int atack = Integer.parseInt(request.getParameter("lvlataque"));
+            int defense = Integer.parseInt(request.getParameter("lvldef"));
+            int speed = Integer.parseInt(request.getParameter("velocidad"));
+            int life = Integer.parseInt(request.getParameter("vida"));
+            
+            
+            Pokemon p = new Pokemon(name,type,hability,atack,defense,speed,life,0);
+            
+            String nameT = request.getParameter("trainer");
+            Trainer entrenadorEscogido = miEjb.getTrainerByName(name);
+            if(miEjb.insertarPokemon(p)){
+                out.println("Pokémon dado de alta");
+            }else{
+                out.println("Este pokémon ya existe");
+            }
             out.println("</body>");
             out.println("</html>");
+            }
+            
         }
     }
 
